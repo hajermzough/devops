df9a30a1d5a18bdbc035994ab508659d25b6ca3d
unknown <mzoughihajer8@gmail.com>
2022-12-02 10:56:44 +0100

    first commit

---
+++ hello-world-/src/test/java/com/formation/integcontinue/AppTest.java
@@ -0,0 +1,20 @@
+package com.formation.integcontinue;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
